792df12436381b077ca924e3f71d2e104837f26d
MJanowskiDev <mat.krz.janowski@gmail.com>
2021-10-16 23:56:25 +0200

    Updated Card component: Added image, and chevron button to show/hide content

--- src/App.js
+++ src/App.js
@@ -6,70 +6,61 @@ import MenuLink from 'components/MenuLink';
 import Menu from 'components/Menu';
 import Employees from 'components/Employees';
 import { getEmployees } from 'utils/eployees/getEployees';
+import paellaImage from 'assets/images/paella.webp';
 
 function App() {
 	return (
-		<div className='app'>
+		<div className="app">
 			<Menu>
-				<MenuLink to='/'>Home</MenuLink>
-				<MenuLink to='/about'>About</MenuLink>
-				<MenuLink to='/contact' isActive>
+				<MenuLink to="/">Home</MenuLink>
+				<MenuLink to="/about">About</MenuLink>
+				<MenuLink to="/contact" isActive>
 					Contact
 				</MenuLink>
-				<MenuLink to='/posts'>Posts</MenuLink>
+				<MenuLink to="/posts">Posts</MenuLink>
 			</Menu>
 			<h1>FORM COMPONENTS</h1>
 			<h4>Buttons</h4>
 			<div>
 				<Button />
-				<Button icon={faCoffee} color='sun-flower' bgColor='wet-asphalt'>
+				<Button icon={faCoffee} color="sun-flower" bgColor="wet-asphalt">
 					Coffe time!
 				</Button>
-				<Button icon={faBookmark} color='wet-asphalt' bgColor='sun-flower'>
+				<Button icon={faBookmark} color="wet-asphalt" bgColor="sun-flower">
 					Bookmark
 				</Button>
 			</div>
 			<h4>Inputs</h4>
 			<div>
 				<Input />
-				<Input color='sun-flower' bgColor='wet-asphalt' />
-				<Input
-					color='sun-flower'
-					bgColor='wet-asphalt'
-					borderColor='asbestos'
-					borderRadius={25}
-					borderSize={5}
-				/>
+				<Input color="sun-flower" bgColor="wet-asphalt" />
+				<Input color="sun-flower" bgColor="wet-asphalt" borderColor="asbestos" borderRadius={25} borderSize={5} />
 			</div>
 			<h4>Textareas</h4>
 			<div>
 				<Textarea />
-				<Textarea color='sun-flower' bgColor='wet-asphalt' />
-				<Textarea
-					color='sun-flower'
-					bgColor='wet-asphalt'
-					borderColor='asbestos'
-					borderRadius={25}
-					borderSize={5}
-				/>
+				<Textarea color="sun-flower" bgColor="wet-asphalt" />
+				<Textarea color="sun-flower" bgColor="wet-asphalt" borderColor="asbestos" borderRadius={25} borderSize={5} />
 			</div>
 			<h4>Cards</h4>
 			<div>
 				<Card
-					title='Shrim and Chorizo Paella'
-					intro='This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'
-					showMore
-					content='Method: Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes....'
+					title="Shrim and Chorizo Paella"
+					intro="This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+					content="Method: Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes...."
+					image={paellaImage}
 				/>
 				<Card
-					title='Shrim and Chorizo Paella'
-					intro='This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'
-					content='Method: Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes....'
+					title="Shrim and Chorizo Paella"
+					intro="This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like."
+					image={paellaImage}
 				/>
 			</div>
 			<h4>Employees</h4>
-			<Employees data={getEmployees()} />
-			<Employees />
+			<div>
+				<Employees data={getEmployees()} />
+				<Employees />
+			</div>
 		</div>
 	);
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { getColorHex } from 'utils/colors/getColor';
 import CardHeader from './CardHeader';
 import CardIntro from './CardIntro';
 import CardContent from './CardContent';
-
+import { faChevronCircleDown, faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const styles = {
 	card: {
 		maxWidth: 345,
@@ -16,14 +18,37 @@ const styles = {
 		display: 'flex',
 		flexDirection: 'column',
 		gap: 16
+	},
+	chevron: {
+		textAlign: 'right',
+		color: getColorHex('sun-flower')
+	},
+	image: {
+		width: '100%',
+		height: 'auto',
+		maxHeight: 200,
+		borderRadius: 6,
+		objectFit: 'cover'
 	}
 };
 
-const Card = ({ title, intro, content, showMore }) => {
+const Card = ({ title, intro, content, image }) => {
+	const [ showMore, setShowMore ] = useState(false);
 	return (
 		<div style={styles.card}>
 			<CardHeader title={title} />
+			<img alt={`${title}`} style={styles.image} src={image} />
 			<CardIntro intro={intro} />
+			{content && (
+				<div style={styles.chevron}>
+					<FontAwesomeIcon
+						onClick={() => setShowMore(!showMore)}
+						size="lg"
+						icon={showMore ? faChevronCircleUp : faChevronCircleDown}
+					/>
+				</div>
+			)}
+
 			{showMore && <CardContent content={content} />}
 		</div>
 	);
